e8d2da0ef0268cfbe79c9f8d618c696576be7191
Kairat Talantbekov <kairat.talantbekov@ext.getpenta.com>
2022-08-17 20:04:03 +0600

    Added javascript execute function and fixed eslint issues

--- helpers/execute.js
+++ helpers/execute.js
@@ -1,26 +1,40 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable no-unused-expressions */
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-const outputPath = path.join(__dirname, "../source_outputs");
+const outputPath = path.join(__dirname, '../source_outputs');
 
 if (!fs.existsSync(outputPath)) {
-  fs.mkdirSync(outputPath, { recursive: true });
+	fs.mkdirSync(outputPath, { recursive: true });
 }
 
 const execute = async (filepath) => {
-  const fileName = path.basename(filepath).split(".")[0];
+	const fileName = path.basename(filepath).split('.')[0];
 
-  return new Promise((resolve, reject) => {
-    exec(`node ./source_codes/${filepath} >> ./source_outputs/${fileName}.txt`, (error, stdout, stderr) => {
-        console.log(error);
-        error && reject({ error, stderr });
-        stderr && reject(stderr);
-        resolve(stdout);
-    });
-  });
+	return new Promise((resolve, reject) => {
+		exec(
+			`node ./source_codes/${filepath} >> ./source_outputs/${fileName}.txt`,
+			async (error, stdout, stderr) => {
+				error && reject({ error, stderr });
+				stderr && reject(stderr);
+				await fs.readFile(
+					`./source_outputs/${fileName}.txt`,
+					'utf8',
+					(err, data) => {
+						const output = data.split('\n');
+						// trim output
+						output.pop();
+						const lastLine = output.pop();
+						resolve(lastLine);
+					}
+				);
+			}
+		);
+	});
 };
 
 module.exports = {
-  execute,
+	execute
 };
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,36 @@
+/* eslint-disable consistent-return */
 /* eslint-disable prefer-destructuring */
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { generateFile } = require("../helpers/generateFile");
-const { execute } = require("../helpers/execute");
+const { generateFile } = require('../helpers/generateFile');
+const { execute } = require('../helpers/execute');
 
 module.exports = (app) => {
-  router.get("/problems", async (req, res) => {
-    res.render("problems");
-  });
+	router.get('/problems', async (req, res) => {
+		res.render('problems');
+	});
 
-  router.get("/problems/:dasdsa", async (req, res) => {
-    res.render("single-problem");
-  });
+	router.get('/problems/:dasdsa', async (req, res) => {
+		res.render('single-problem');
+	});
 
-  router.post("/run", async (req, res) => {
-    const { language = "js", code } = req.body;
+	router.post('/run', async (req, res) => {
+		const { language = 'js', code } = req.body;
 
-    console.log(language, "Length:", code.length);
+		if (code === undefined) {
+			return res
+				.status(400)
+				.json({ success: false, error: 'Empty code body!' });
+		}
+		// need to generate a file with content from the request
+		const filepath = await generateFile(language, code);
+		try {
+			const output = await execute(filepath);
+			res.status(201).json({ message: output });
+		} catch (err) {
+			res.status(201).json({ message: err.stderr });
+		}
+	});
 
-    if (code === undefined) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Empty code body!" });
-    }
-    // need to generate a file with content from the request
-    const filepath = await generateFile(language, code);
-    const output = await execute(filepath);
-    console.log('=====================')
-    console.log(output);
-    console.log('=====================')
-
-    res.status(201).json("OK");
-  });
-
-  app.use("/", router);
+	app.use('/', router);
 };
