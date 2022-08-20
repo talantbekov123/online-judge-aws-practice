3a6af682a6037f700a420b68829b0298aa91d91d
Kairat Talantbekov <kairat.talantbekov@ext.getpenta.com>
2022-08-21 01:45:32 +0600

    Added some admin views

---
+++ routes/admins.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-destructuring */
+const express = require('express');
+const router = express.Router();
+
+module.exports = (app) => {
+	router.get('/problems', async (req, res) => {
+		res.render('./admins/problems');
+	});
+
+	router.get('/testcases', async (req, res) => {
+		res.render('./admins/testcases');
+	});
+
+	app.use('/admins', router);
+};
---
+++ routes/testcases.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-destructuring */
+const express = require('express');
+const router = express.Router();
+
+module.exports = (app) => {
+	router.get('/', async (req, res) => {
+		res.send('OK');
+	});
+
+	app.use('/testcases', router);
+};
